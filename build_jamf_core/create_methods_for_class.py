370365e1d1d114d34ec1fd30e11399cf84cfc2ea
Jeremy Rodrigues <jeremy.rodrigues@therealreal.com>
2019-08-03 09:31:26 -0700

    Now all urls split on slash that equal 4 are now accounted for.

--- build_jamf_core/create_methods_for_class.py
+++ build_jamf_core/create_methods_for_class.py
@@ -9,10 +9,18 @@ class MethodLogic:
     base_url = os.environ["JAMF_URL_PROD"]
     key = os.environ["JAMF_KEY"]
     username = os.environ["JAMF_USERNAME"]
+    jamf_id = None  # if
 
     def __init__(self, url, rest_type):
         self.url = url
         self.rest_type = rest_type
+
+    def separate_url_by_slash(self):
+        """
+        Divides the url up by each /
+        :return:
+        """
+        return self.url.split("/")
 
     def is_status_200(self):
         """
@@ -43,18 +51,32 @@ class MethodLogic:
 
     def is_base_url(self):
         """
-        Validate the url is the base url. Example is Class Accounts has a url /accounts that returns basic info like id
-        and name.
+        Validate that the url is the base url. Example is Class Accounts has a base url /accounts that returns basic
+        info like id and name of each account.
         :return:
         """
-        separate_url_by_slash = self.url.split("/")
 
-        if len(separate_url_by_slash) == 2:  # Example /accounts len is equal to 2.
+        if len(self.separate_url_by_slash()) == 2:  # Example /accounts len is equal to 2.
             base_url = True
         else:
             base_url = False
-            
+
         return base_url
+
+    def is_simple_search_url(self):
+        """
+        Validate that the url is the base url. Example is Class Accounts has a base url /accounts that returns basic
+        info like id and name of each account.
+        :return:
+        """
+
+        if len(self.separate_url_by_slash()) == 4:  # Example /accounts len is equal to 2.
+            base_url = True
+        else:
+            base_url = False
+
+        return base_url
+
 
     def create_func_text_for_base_url(self):
         f_string_handler = 'f"{self.url}'
@@ -64,9 +86,25 @@ class MethodLogic:
 
         return text
 
+    def create_func_text_for_simple_search_url(self):
+        f_string_handler = 'f"{self.url}'
+        func_title = self.separate_url_by_slash()[2]
+        search_paramater = self.separate_url_by_slash()[3].replace("{", "").replace("}", "")
+        url_extension = self.url.replace(f"/{self.separate_url_by_slash()[1]}", "")
+
+        text = f'    def by_{func_title}(self, {search_paramater}):\n' \
+            f'        self.url = {f_string_handler}{url_extension}"\n' \
+            f'        return self.get_jamf()\n\n'
+
+        return text
+
     def main(self):
         func_text = None
         if self.is_base_url() and self.is_status_200():
             func_text = self.create_func_text_for_base_url()
 
+        elif self.is_simple_search_url():
+            func_text = self.create_func_text_for_simple_search_url()
+
         return func_text
+
--- build_jamf_core/dev_create_class.py
+++ build_jamf_core/dev_create_class.py
@@ -11,6 +11,9 @@ class CreateClass:
         self.url_index = url_index
 
     def text_base(self):
+        """
+        This creates the imports and basic class sturcutre
+        """
         text = f"from core.get_jamf.get_jamf import GetJamf\n\n\n" \
             f"class {self.url_index[0][0].replace('/', '').capitalize()}(GetJamf):\n\n" \
             f"    url = '{self.url_index[0][0]}'\n\n"
@@ -18,6 +21,10 @@ class CreateClass:
         return text
 
     def create_file(self):
+        """
+        Creates the python file and writes the class and its methods.
+        :return:
+        """
         file_name = self.url_index[0][0].replace("/", "") + ".py"
 
         with open(Path(f"core/get_jamf/{file_name}"), "w") as writefile:
@@ -30,12 +37,18 @@ class CreateClass:
             url = url_data[0]
             rest_type = url_data[1]
             func_text = MethodLogic(url, rest_type).main()
-            if func_text:
-                print(f"--{url_data}")
-                self.class_text += func_text
-                create_file_flag = True  # I only need to create files if func_data is returned
+            if rest_type == 'get':
+                if func_text and not create_file_flag:  # If this condition is met then create the file
+                    print(f"--{url_data}")
+                    self.class_text += func_text
+                    create_file_flag = True  # I only need to create files if func_data is returned
+
+                elif func_text and create_file_flag:  # if file is being created then add this method
+                    print(f"--{url_data}")
+                    self.class_text += func_text
+
 
         if create_file_flag:
             self.create_file()
-            
+
 
